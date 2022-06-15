b2949c3624182aa7a222e47fee3d59e2dded8258
HITENDRAPAL3 <hitendrasinghpal3@gmail.com>
2022-06-15 12:11:51 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    
+    <div className="footer-container">
+      <p>2022 S&H STORE</p>
+      <p>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ pages/canceled.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+const canceled = () => {
+  return (
+   <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+        </p>
+        <h2>Sorry, Your Order was Cancelled !!</h2>
+        
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:S&H@gmail.com">
+            S&H@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default canceled
